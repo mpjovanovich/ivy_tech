d5b2020d4db351eef449edbbe9c36fb3a66a0371
Mike Jovanovich <mpjovanovich@gmail.com>
2022-10-30 17:30:26 -0400

    getting close...

--- SDEV120_Computing_Logic/programming_exercise_grader.sh
+++ SDEV120_Computing_Logic/programming_exercise_grader.sh
@@ -9,19 +9,21 @@ read -p "Number of program runs: " num_program_runs
 read -p "Number of program inputs: " num_program_inputs 
 
 out_path=${student_file_path}/out
+tmp_path=${student_file_path}/tmp
 solution_log_path=${out_path}/solution_log.txt
 rm -r $out_path
 mkdir $out_path
+rm -r $tmp_path
+mkdir $tmp_path
 
 for(( i = 1; i <= $num_program_runs; i++ ));
 do
-    input_array=()
+    touch ${tmp_path}/tmp${i}
     for(( j = 1; j <= $num_program_inputs; j++ ));
     do
-        read -p "Input value, run ${i}, input $j: " input_value
-        input_array+=($input_value)
+        read -p "Input value, run $i, input $j: " input_value
+        echo $input_value >> ${tmp_path}/tmp${i}
     done
-    printf "%s\n" "${input_array[@]}" > tmp${i}
 done
 
 ##############################################################################
@@ -34,25 +36,28 @@ for(( i = 1; i <= $num_program_runs; i++ ));
 do
     ## cat tmp file to bash | read one line at a time with a pause in between | print to stdout and pipe 
     ## to the solution file > redirect stdout to /dev/null
-    script -c "cat tmp${i} | { while read l ; do sleep .1; echo \$l; done } | tee >(python3 $grade_program_path)" \
+    script -c "cat ${tmp_path}/tmp${i} | { while read l ; do sleep .1; echo \$l; done } | tee >(python3 $grade_program_path)" \
         -O ${solution_log_path}${i} > /dev/null
     ## The -q option doesn't seem to work in script, so we need to remove the first and last lines
-    tail -n +2 ${solution_log_path}${i} | head -n -1 > tmp 
-    mv tmp ${solution_log_path}${i}
+    tail -n +2 ${solution_log_path}${i} | head -n -1 > ${tmp_path}/tmp 
+    mv ${tmp_path}/tmp ${solution_log_path}${i}
 done
 stty opost
 
 echo -n '' > ${solution_log_path}
-for(( i = 1; i <= $num_program_runs; i++ ));
+for(( i=1; i<=$num_program_runs; i++ ));
 do
-    echo "BEGIN PROGRAM RUN ${i}" >> ${solution_log_path}
+    echo -n "PROGRAM RUN ${i}: {" >> ${solution_log_path}
+    cat ${tmp_path}/tmp${i} | { while read l ; do echo -n "$l "; done } >> ${solution_log_path}
+    echo "}" >> ${solution_log_path}
+
     cat ${solution_log_path}${i} >> ${solution_log_path}
     rm ${solution_log_path}${i}
 done
 
 ## I'm not sure where the extra LFs are coming from but they bother me.
 ## Let's get rid of them.
-truncate -s-2 ${solution_log_path}
+truncate -s-1 ${solution_log_path}
 
 
 ##############################################################################
@@ -64,7 +69,7 @@ do
     ## Use parameter expansion to only return the file name - not the full path.
     ## Store it in a variable.
     student_name=$(echo "${student_file##*/}" | cut -d '_' -f 1)
-    student_out_file=${out_path}/${student_name}_log.txt
+    student_log_path=${out_path}/${student_name}_log.txt
     student_diff_file=${out_path}/${student_name}_diff.txt
 
     ## Change terminal new line from CRLF to LF, then change back after the script
@@ -73,34 +78,30 @@ do
     do
         ## cat tmp file to bash | read one line at a time with a pause in between | print 
         ## to stdout and pipe to the student file > redirect stdout to /dev/null
-        script -c "cat tmp${i} | { while read l ; do sleep .1; echo \$l; done } | tee >(python3 $student_file)" \
-            -O ${student_out_file}${i} > /dev/null
+        script -c "cat ${tmp_path}/tmp${i} | { while read l ; do sleep .1; echo \$l; done } | tee >(python3 $student_file)" \
+            -O ${student_log_path}${i} > /dev/null
         ## The -q option doesn't seem to work in script, so we need to remove the first and last lines
-        tail -n +2 ${student_out_file}${i} | head -n -1 > tmp 
-        mv tmp ${student_out_file}${i}
+        tail -n +2 ${student_log_path}${i} | head -n -1 > ${tmp_path}/tmp 
+        mv ${tmp_path}/tmp ${student_log_path}${i}
     done
     stty opost
     
-    echo -n '' > ${student_out_file}
+    echo -n '' > ${student_log_path}
     for(( i = 1; i <= $num_program_runs; i++ ));
     do
-        echo "BEGIN PROGRAM RUN ${i}" >> ${student_out_file}
-        cat ${student_out_file}${i} >> ${student_out_file}
-        rm ${student_out_file}${i}
+        echo -n "PROGRAM RUN ${i}: {" >> ${student_log_path}
+        cat ${tmp_path}/tmp${i} | { while read l ; do echo -n "$l "; done } >> ${student_log_path}
+        echo "}" >> ${student_log_path}
+
+        cat ${student_log_path}${i} >> ${student_log_path}
+        rm ${student_log_path}${i}
     done
 
     ## I'm not sure where the extra LFs are coming from but they bother me.
     ## Let's get rid of them.
-    truncate -s-2 ${student_out_file}
+    truncate -s-1 ${student_log_path}
 
     ## Create the diff file
-    diff --side-by-side --suppress-common-lines ${solution_log_path} $student_out_file > $student_diff_file
-    ##diff ${solution_log_path} $student_out_file > $student_diff_file
+    diff --side-by-side --suppress-common-lines $solution_log_path $student_log_path > $student_diff_file
 
-done
-
-## Cleanup
-for(( i = 1; i <= $num_program_runs; i++ ));
-do
-    rm tmp${i}
 done
