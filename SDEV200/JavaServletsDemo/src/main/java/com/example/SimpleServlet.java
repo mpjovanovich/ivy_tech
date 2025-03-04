fdeead4a2332a16c2e557416b9c4f94cd693852d
Mike Jovanovich <mpjovanovich@gmail.com>
2025-03-04 08:57:38 -0500

    Add files via upload

---
+++ SDEV200/JavaServletsDemo/src/main/java/com/example/SimpleServlet.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class SimpleServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h1>I am a test Servlet!</h1>");
+        out.println("<p>The time is: " + new java.util.Date() + "</p>");
+        out.println("</body></html>");
+    }
+}
